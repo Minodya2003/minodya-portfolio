c012f12ab3a74017278d8eebbc8147c304a422bd
buddhimasanthush <buddhimasanthush@gmail.com>
2026-06-13 09:43:14 +0530

    Update project and add animation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HeroDemo from '@/components/demo'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-background font-sans antialiased">
+      <HeroDemo />
+    </main>
+  )
+}
+
+export default App
